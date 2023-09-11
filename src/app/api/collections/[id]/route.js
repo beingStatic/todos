8fcaf2da2c583bc309c14b9e3a225d4e3775ea9c
beingStatic <mohammedsufiyan23@gmail.com>
2023-09-11 23:22:14 +0530

    Nothing

--- src/app/api/collections/[id]/route.js
+++ src/app/api/collections/[id]/route.js
@@ -2,17 +2,17 @@ import { NextResponse } from 'next/server';
 import connectMongoDB from '../../../../../libs/mongodb';
 import Collection from '../../../../../models/collection';
 
+export async function GET(request, { params }) {
+  const { id } = params;
+  await connectMongoDB();
+  const collection = await Collection.findOne({ _id: id });
+  return NextResponse.json({ collection }, { status: 200 });
+}
+
 export async function PUT(request, { params }) {
   const { id } = params;
   const { newTitle: title, newContent: content } = await request.json();
   await connectMongoDB();
   await Collection.findByIdAndUpdate(id, { title, content });
   return NextResponse.json({ message: 'Collection Updated' }, { status: 200 });
-}
-
-export async function GET(request, { params }) {
-  const { id } = params;
-  await connectMongoDB();
-  const collection = await Collection.findOne({ _id: id });
-  return NextResponse.json({ collection }, { status: 200 });
 }
--- src/app/api/collections/route.js
+++ src/app/api/collections/route.js
@@ -1,18 +1,17 @@
 import { NextResponse } from 'next/server';
 import connectMongoDB from '../../../../libs/mongodb';
 import Collection from '../../../../models/collection';
+export async function GET() {
+  await connectMongoDB();
+  const collections = await Collection.find();
+  return NextResponse.json({ collections });
+}
 
 export async function POST(request) {
   const { title, content } = await request.json();
   await connectMongoDB();
   await Collection.create({ title, content });
   return NextResponse.json({ message: 'Collecton created' }, { status: 200 });
-}
-
-export async function GET() {
-  await connectMongoDB();
-  const collections = await Collection.find();
-  return NextResponse.json({ collections });
 }
 
 export async function DELETE(request) {
